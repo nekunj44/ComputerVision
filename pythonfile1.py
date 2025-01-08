6da8d9ee8ca2f7b09d1e1b2760fa2745e70c022d
nekunj44 <kuvam654@gmail.com>
2025-01-08 14:22:09 +0530

    repo updated

---
+++ pythonfile1.py
@@ -0,0 +1,7 @@
+import cv2
+
+img1 = cv2.imread("/home/nekunj44/Pictures/Screenshots/merc77.jpg")
+print(img1)
+#it will output in form of [Blue Intensity, Green Intensity, Red Intensity]
+
+print(img1.shape)
