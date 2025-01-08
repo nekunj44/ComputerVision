3c5ff38cc6c25f6d53c37bc4d860c41ac570dc7f
nekunj44 <kuvam654@gmail.com>
2025-01-08 15:05:05 +0530

    updated file

--- pythonfile1.py
+++ pythonfile1.py
@@ -2,6 +2,14 @@ import cv2
 
 img1 = cv2.imread("/home/nekunj44/Pictures/Screenshots/merc77.jpg")
 print(img1)
-#it will output in form of [Blue Intensity, Green Intensity, Red Intensity]
+#it will output in form of [Blue Intensity, Green Intensity, Red Intensity] if coloured image
+#[[]] means one complete row of pixels.
 
 print(img1.shape)
+#it will output in form of (height/rows, width/columns, channel)
+#channel can be 3 (if its colored image) or 1 (if its grayscale image)
+
+cv2.imshow("newwindow", img1)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
